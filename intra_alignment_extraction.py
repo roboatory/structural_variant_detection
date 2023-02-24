bcbdf6bed506754b5c6facfcd8ec3da50b144011
Rkhurana19 <khuranarohit75@gmail.com>
2023-02-23 23:11:37 -0600

    added vectorization and visualization

--- intra_alignment_extraction.py
+++ intra_alignment_extraction.py
@@ -1,17 +1,66 @@
 from argparse import ArgumentParser
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import pysam
 import re
 
-def intra_alignment_extraction(bam_file, bed_file, variant = None, extension = 50):
+def visualize_alignments(bed, variant, images):
+    chromosome = variant[0]
+    del_start_position = variant[1]
+    del_end_position = variant[1] + variant[2]
+    
+    read_alignments = pd.read_csv(bed + "/{}_{}_{}.bed".format(chromosome, 
+                          del_start_position, del_end_position), sep = "\t")
+    read_alignments["HEIGHT"] = read_alignments.groupby("READ").ngroup() + 2
+
+    plt.plot([del_start_position, del_end_position], [1, 1], color = "blue")
+    for _, row in read_alignments.iterrows():
+        plt.plot([row["START"], row["END"]], [row["HEIGHT"], row["HEIGHT"]], color = "orange")
+
+    # TODO: make figure nicer
+    
+    plt.savefig(images + "/{}_{}_{}.png".format(chromosome, del_start_position, del_end_position))
+
+def visualize_matrix(matrix, variant, images):
+    chromosome = variant[0]
+    del_start_position = variant[1]
+    del_end_position = variant[1] + variant[2]
+
+    # TODO: make extensible colormap
+
+    color_map = ListedColormap(["r", "g"])
+    plt.matshow(matrix, cmap = color_map)
+    plt.savefig(images + "/{}_{}_{}_matrix.png".format(chromosome, del_start_position, del_end_position))
+
+def vectorize(variant, extension_length, read_sv_start, read_sv_length, read_vector):
+    del_start_position = variant[1]
+    del_end_position = variant[1] + variant[2]
+
+    read_sv_end = read_sv_start + read_sv_length
+
+    read_sv_rep_start = max(del_start_position - extension_length, read_sv_start) - (del_start_position - extension_length)
+    read_sv_rep_end = min(del_end_position + extension_length, read_sv_end) - (del_start_position - extension_length)
+
+    mask = [1] * (read_sv_rep_end - read_sv_rep_start)
+    read_vector[read_sv_rep_start : read_sv_rep_end] = mask
+
+    return True
+
+def intra_alignment_extraction(bam_file, bed, variant = None, extension = 50):
     # if user supplies VCF file, parse sig contents (defined below)
     if variant:
         chromosome = variant[0]
         del_start_position = variant[1]
         del_end_position = variant[1] + variant[2]
 
-        bed = open(bed_file, "w")
+        bed_file = open(bed + "/{}_{}_{}.bed".format(chromosome, del_start_position, del_end_position), "w")
+        bed_file.write("{}\t{}\t{}\t{}\t{}\n".format("CHROMOSOME", "START", "END", "READ", "TYPE"))
 
         sam_file = pysam.AlignmentFile(bam_file, "rb")
+        image_matrices = []
+
         for read in sam_file.fetch(chromosome, until_eof = True):
             # get start and end position of aligned reads relative to the reference
             read_start = read.reference_start
@@ -22,19 +71,27 @@ def intra_alignment_extraction(bam_file, bed_file, variant = None, extension = 5
                 current_read_position = read_start
                 cigar_string = read.cigartuples
 
+                read_vector = [0] * ((del_end_position - del_start_position) + 2 * extension)
+                encountered_overlap = False
+
                 for op, length in cigar_string:
                     if op == 2: # "D"
                         # check for overlap between CIGAR deletion and proposed variant in VCF file
                         if (max(current_read_position, del_start_position - extension) <= 
                             min(current_read_position + length, del_end_position + extension)):
-                            bed.write("DEL\t" + chromosome + "\t" + str(current_read_position) + 
-                                        "\t" + str(current_read_position + length) + "\n")
+                            encountered_overlap = vectorize(variant, extension, current_read_position, length, read_vector)
+                            bed_file.write(chromosome + "\t" + str(current_read_position) + "\t" + str(current_read_position 
+                                               + length) + "\t" + read.query_name + "\t" + "DEL" + "\n")
                         current_read_position += length
 
                     elif op == 0: # "M"
                         current_read_position += length
 
-        bed.close()
+                if encountered_overlap:       
+                    image_matrices.append(read_vector)
+
+        bed_file.close()
+        return image_matrices
 
 def parse_vcf_file(vcf_file, chromosome):
     with open(vcf_file, "r") as file:
@@ -53,7 +110,8 @@ def parse_args():
 
     parser.add_argument("-b", "--bam", default = "datasets/chr21.bam", help = "user-supplied BAM file (default: datasets/chr21.bam)")
     parser.add_argument("-c", "--chromosome", default = "chr21", help = "limits signature extraction to a particular chromosome (default: chr21)")
-    parser.add_argument("-d", "--bed", default = "datasets/chr21.bed", help = "output BED file (default: chr21.bed)")
+    parser.add_argument("-d", "--bed", default = "datasets/bed", help = "output BED directory (default: datasets/bed)")
+    parser.add_argument("-i", "--images", default = "datasets/images", help = "output image directory (default: datasetes/images")
     parser.add_argument("-v", "--vcf", default = "datasets/fp.vcf", help = "user-supplied VCF file (default: datasets/fp.vcf)")
 
     return parser.parse_args()
@@ -62,13 +120,16 @@ def main():
     args = parse_args()
 
     bam_file = args.bam
-    bed_file = args.bed
+    bed = args.bed
     chromosome = args.chromosome
     vcf_file = args.vcf
+    images = args.images
 
     variants = parse_vcf_file(vcf_file, chromosome)
     
     for variant in variants:
-        intra_alignment_extraction(bam_file, bed_file, variant)
+        intra_variant_matrix = intra_alignment_extraction(bam_file, bed, variant)
+        visualize_alignments(bed, variant, images)
+        visualize_matrix(intra_variant_matrix, variant, images)
 
 main()
